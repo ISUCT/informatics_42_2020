6b23e6c3d3110c8732bfc1b52bbdf1d764d90633
alexthvest <alexthvest@gmail.com>
2020-10-03 20:28:07 +0300

    Solved function  №6

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,23 +1,63 @@
 #include <iostream>
 #include <math.h>
+#include <vector>
 
-double func1(const double x)
+double func(const double x)
 {
-    const double b = 2.5;
-    const double axPow = pow(b, 3) + pow(x, 3);
+    if (abs(x) < 1)
+        return acos(x);
 
-    const double numerator = 1 + pow(sin(axPow), 2);
-    const double denominator = pow(axPow, 1 / 3);
+    return pow(1.2, x) - pow(x, 1.2);
+}
 
-    return numerator / denominator;
+std::vector<double> exampleA(const double xStart, const double xEnd, const double xDelta)
+{
+    std::vector<double> results;
+
+    for (double x = xStart; x <= xEnd; x += xDelta)
+    {
+        double result = func(x);
+        results.push_back(result);
+    }
+
+    return results;
+}
+
+std::vector<double> exampleB(const double *values, const int size)
+{
+    std::vector<double> results;
+
+    for (int i = 0; i < size; i++)
+    {
+        double result = func(values[i]);
+        results.push_back(result);
+    }
+
+    return results;
+}
+
+void printExampleInfo(const char *title, std::vector<double> results)
+{
+    std::cout << "=========== " << title << " ===========" << std::endl;
+    std::cout << "Results: ";
+
+    for (double result : results)
+    {
+        std::cout << result << "\t";
+    }
+
+    std::cout << std::endl;
 }
 
 int main()
 {
-    for (double x = 1.28; x <= 3.28; x += 0.4)
-    {
-        std::cout << "x = " << func1(x) << std::endl;
-    }
+    auto exampleAResults = exampleA(0.2, 2.2, 0.4);
+    printExampleInfo("Example A", exampleAResults);
+
+    double values[] = {0.1, 0.9, 1.2, 1.5, 2.3};
+    auto exampleBResults = exampleB(values, sizeof(values) / sizeof(double));
+
+    printExampleInfo("Example B", exampleBResults);
 
     return 0;
 }
