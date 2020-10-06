35cf9250be6083b16aaa8c2105661e897c494baf
SanyaKirilv <71167144+SanyaKirilv@users.noreply.github.com>
2020-10-06 10:50:37 +0300

    Testing code

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -2,10 +2,23 @@
 //
 
 #include <iostream>
+#include <cmath>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+
+    const double a = 2.0;
+    const double b = 4.1;
+    double x = 0.77;
+    for (x; x <= 1.77; x = x + 0.2)
+    {
+        double numerator = log((pow(b, 2) - pow(x, 2))) / log(a);
+        double denuminator = pow(abs(pow(x, 2) - pow(a, 2)), 1 / 3.0);
+        double y = numerator / denuminator;
+        std::cout << "x = " << x << std::endl;
+        std::cout << "y = " << y << std::endl;
+    }
+
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
