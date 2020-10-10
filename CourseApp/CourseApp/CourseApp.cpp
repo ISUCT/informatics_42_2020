639ebd3bdff930323b75aa1b6821ee471830cd42
Vladimir Kozhukhar <mis7akes@mail.ru>
2020-10-10 10:19:19 +0300

    the second part of laboratory work

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -9,14 +9,11 @@ int main()
         double y = pow(a,pow(x,2)-1) - log10(pow(x,2)-1) + pow(pow(x,2)-1, 1/3.0);
         std::cout << "x= "<< x << " y= " << y <<std::endl;
     }
+    std:: cout << "------------------------------------------"<< std::endl;
+    double  xs[5] = {1.28, 1.36, 2.47, 3.68, 4.56};
+    for (int i = 0 ; i < 5; i++)  {
+        double x = xs[i];
+        double y = pow(a,pow(x,2)-1) - log10(pow(x,2)-1) + pow(pow(x,2)-1, 1/3.0);
+        std::cout << "x= "<< x << " y= " << y <<std::endl;
+    }
 }
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
