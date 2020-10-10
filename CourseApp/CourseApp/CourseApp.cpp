90e7c7309c33527ebf71aa663ca92767367839bf
User of A315 <a315@it.isuct.ru>
2020-10-10 10:56:07 +0400

    First lab

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,20 +1,28 @@
-// CourseApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cmath>
+using namespace std;
+
+double a = 2.0;
+double b = 0.95; 
+double e = exp(1.0);
+
+double y(double c) {
+	double d1 = 1 + (log(c / a)*log(c / a));
+	double d2 = b - pow(e, (c / a));
+	return d1 / d2;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	for (double x = 1.25; x <= 2.75; x += 0.3) {
+		cout << y(x) << endl;
+	}
+
+	double m[5] = { 2.2, 3.78, 4.51, 6.58, 1.2 };
+	for (int i = 0; i < 5; i++) {
+		cout << y(m[i]) << endl;
+	}
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
