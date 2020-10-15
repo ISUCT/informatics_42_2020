cc7f74c2c2f463580fe5021e131c7c12daadee50
Vladimir Kozhukhar <mis7akes@mail.ru>
2020-10-15 19:06:20 +0300

    some useless changes

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -7,19 +7,19 @@ double func(double x, const double a = 1.6)
 }
 int main()
 {
+    std::cout << "Task A" << std::endl;
     double dx = 0.5;
     for (double x = 1.2 ; x <= 3.7; x += dx) 
     {
-        func(x);
         double y = func(x);
         std::cout << "x= "<< x << " y= " << y <<std::endl;
     }
     std::cout << "------------------------------------------"<< std::endl;
+    std::cout << "Task B" << std::endl;
     double  xs[5] = {1.28, 1.36, 2.47, 3.68, 4.56};
     for (int i = 0 ; i < 5; i++)  
     {
         double x = xs[i];
-        func(x);
         double y = func(x);
         std::cout << "x= "<< x << " y= " << y <<std::endl;
     }
