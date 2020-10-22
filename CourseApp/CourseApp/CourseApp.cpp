e2760dc435bec2556b207a194b8372929cee2280
Mark_Rumyantsev <mrk.ru@list.ru>
2020-10-22 19:48:42 +0300

    Task A and B

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,30 +1,36 @@
-// CourseApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
-#include <cmath>
+#include "Calc.h"
 using namespace std;
-int main()
-{
-	const double a = 0.8;
-	const double b = 0.4;
-	for (double x = 1.23; x <= 7.23; x += 1.2) {
-		double numenator = pow(pow(x - a, 2), 1 / 3.0) + pow(abs(x + b), 1 / 5.0);
-		double denominator = pow(pow(x, 2) - pow(a - b, 2), 1 / 9.0);
-		double y = numenator / denominator;
-		cout << "x= "<< x <<  " y= " << y << endl;
+
+	 int main(int argc,char *argv[])
+	 {
+		 cout << "We have " << argc << " params" << endl;
+		 for (int i = 0; i < argc;i++) 
+		 {
+			 cout << argv[i] << endl;
+		 }
+
+
+		 const double a = 0.8;
+		 const double b = 0.4;
+		 taskA(a, b, 1.23, 7.23, 1.2);
+		 double x[] = { 1.88 ,2.26 ,3.84 ,4.55 ,-6.21 };
+		 cout << "----------------------" << endl;
+		 taskB(a, b, x, sizeof(x)/sizeof(x[0]));
+		 char end;
+		 cin >> end;
+		 return 0;
+		
+			
+		/*int tmp = 45;  œ–»—¬¿»¬¿Õ»≈ ” ¿«¿“≈Àﬂ
+		int *link = &tmp;
+		cout << tmp << "\t" << &tmp << "\t" << link << "\t" << *link << endl;*/
+	
+
 	}
 
-}
 
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
