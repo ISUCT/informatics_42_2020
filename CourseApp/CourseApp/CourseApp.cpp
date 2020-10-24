b1a06243dcc423fcb68c2a5d9ee2fd5c76140be2
Eugeny KOnstantinov <jskonst@yandex.ru>
2020-10-24 10:31:00 +0300

    Add sample lab

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,32 +1,29 @@
 // CourseApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
-#include <cmath>
+#include "Calc.h"
 
-int main()
-{
-	const double a = 2.0;
-	const double b = 4.1;
-
-	double dx = 0.2;
-	for (double x = 0.77; x <= 1.77; x += dx) {
-		double numenator = log(pow(b, 2) - pow(x, 2)) / log(a);
-		double denominator = pow(abs(pow(x, 2) - pow(a, 2)), 1 / 3.0);
-		double y = numenator / denominator;
-		std::cout << "x= " << x << " y=" << y << std::endl;
+void print(int(*numbers)[2], const int rows, int columns) {
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			std::cout << numbers[i][j] << "\t";
+		}
+		std::cout << std::endl;
 	}
-
-
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void increment(int *x)
+{
+	(*x)++;
+	std::cout << "increment function: " << *x << std::endl;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main(int argc, char *argv[])
+{
+	int nums[3][2] = { {1, 22}, {13, 54}, {75,46} };
+	print(nums, 3, 2);
+	return 0;
+}
+
