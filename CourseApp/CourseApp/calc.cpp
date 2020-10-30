514ce7e2637f9df08111096ac854d48d3c696f98
Mark_Rumyantsev <mrk.ru@list.ru>
2020-10-30 16:40:17 +0300

    arithmetic A and B

--- CourseApp/CourseApp/calc.cpp
+++ CourseApp/CourseApp/calc.cpp
@@ -11,21 +11,44 @@ double calc(double a, double b, double x)
 	double y = numenator / denominator;
 	return y;
 }
-double calc(double x)
+
+void taskA(double xn, double xk, double dx, int* pSizeA)
 {
-	const double a = 0.8;
-	const double b = 0.4;
-	return calc(a, b, x);
+	int numer = *pSizeA;
+	for (double x = xn; x <= xk; x += dx)
+	{
+		numer++;
+	}
+	*pSizeA = numer;
 }
-void taskA(double a, double b, double xn, double xk, double dx)
+void ScoreTaskA(double a, double b, double xn, double xk, double dx, double *ArrayA)
 {
-	for (double x = xn; x <= xk; x += dx) {
+	int i = 0;
+	for (double x = xn; x <= xk; x += dx)
+	{
 		double y = calc(a, b, x);
-		cout << x << "\t" << y << endl;
+		ArrayA[i] = x;
+		i++;
+		ArrayA[i] = y;
+		i++;
 	}
 	return;
 }
-void taskB(double a, double b, double *x, int size)
+void ScoreTaskB(double a, double b, double* x, int SizeB, double* ArrayB)
+{
+	int i = 0;
+	for (int n = 0; n <= ((SizeB / 2) - 1); n++)
+	{
+		double y = calc(a, b, x[n]);
+		ArrayB[i] = x[n];
+		i++;
+		ArrayB[i] = y;
+		i++;
+	}
+	return;
+}
+
+/*void taskB(double a, double b, double *x, int size)
 {
 	for (int i = 0;i < size;i++)
 	{
@@ -34,4 +57,4 @@ void taskB(double a, double b, double *x, int size)
 		sprintf_s(result, "x = %.3f y = %.3f \n", x[i], y);
 		cout << result << endl;
 	}
-}
+} */
