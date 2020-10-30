9b1abbcb1b369d2a14918f42fc2152413b5ed342
Mark_Rumyantsev <mrk.ru@list.ru>
2020-10-30 16:41:03 +0300

    output dynamic

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include "Calc.h"
+#include <cstdlib>
 using namespace std;
 
 	 int main(int argc,char *argv[])
@@ -12,15 +13,32 @@ using namespace std;
 			 cout << argv[i] << endl;
 		 }
 
-
+		 int SizeA = 0;
+		 int SizeB = 0;
+		 int* pSizeA = &SizeA;
+		 double x[] = { 1.88 ,2.26 ,3.84 ,4.55 ,-6.21 };
 		 const double a = 0.8;
 		 const double b = 0.4;
-		 taskA(a, b, 1.23, 7.23, 1.2);
-		 double x[] = { 1.88 ,2.26 ,3.84 ,4.55 ,-6.21 };
-		 cout << "----------------------" << endl;
-		 taskB(a, b, x, sizeof(x)/sizeof(x[0]));
+		 taskA( 1.23, 7.23, 1.2,pSizeA);
+		 SizeA = SizeA * 2;
+		 double* ArrayA = new double[SizeA];
+		 SizeB = ((sizeof(x) / sizeof(x[0])) * 2);
+		 double* ArrayB = new double[SizeB];
+		 ScoreTaskA(a, b, 1.23, 7.23, 1.2, ArrayA);
+		 for (int i = 0; i <= (SizeA - 1); i += 2)
+		 {
+			 cout << "x = " << ArrayA[i] << " | " << "y = " << ArrayA[i + 1] << endl;
+		 }
+		 cout << "--------------" << endl;
+		 ScoreTaskB(a, b, x, SizeB, ArrayB);
+		 for (int j = 0; j <= (SizeB - 1); j += 2)
+		 {
+			 cout << "x = " << ArrayB[j] << " | " << "y = " << ArrayB[j + 1] << endl;
+		 }
 		 char end;
 		 cin >> end;
+		 delete[] ArrayA;
+		 delete[] ArrayB;
 		 return 0;
 		
 			
