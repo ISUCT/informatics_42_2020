f706e44bb4a3fcb75cf8059048bfc89517341bfa
dmitriy-malakhov <72221043+dmitriy-malakhov@users.noreply.github.com>
2020-10-31 11:39:37 +0400

    Delete Calc.h

--- CourseApp/CourseApp/Calc.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-double calc(double a, double b, double x);
-void taskA(double xn, double xk, double dx, int* pSizeA);
-void ScoreTaskA(double a, double b, double xn, double xk, double dx, double* ArrayA);
-void ScoreTaskB(double a, double b, double* x, int SizeB, double* ArrayB);
