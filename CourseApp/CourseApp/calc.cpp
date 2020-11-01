14b5a4bab583bad5caa463d08b797d7a1d8a4e5a
elizabetherm3@gmail.com <elizabetherm3@gmail.com>
2020-11-01 23:58:33 +0300

    Array taskA and taskB

--- CourseApp/CourseApp/calc.cpp
+++ CourseApp/CourseApp/calc.cpp
@@ -12,29 +12,25 @@ double calc(double a, double b, double x)
 	double y = numenator/denominator;
 	return y;
 }
-double calc(double x)
-{
-	const double a = 1.2;
-	const double b = 0.48;
 
-	return calc(a, b, x);
-}
 void taskA(double a, double b, double xn, double xk, double dx)
 {
+	int n = (xk - xn) / dx;
+	double *y = new double[n];
+	int i = 0;
 	for (double x = xn; x <= xk; x += dx)
 	{
-		double y = calc(a, b, x);
-		std::cout << "x = " << x << " y = " << y << std::endl;
+		y[i] = calc(a, b, x);
+		cout << y[i] << "\t" << endl;
+		i++;
 	}
 	return;
 }
-void taskB(double a, double b, double* x, int size)
+void taskB(double a, double b, double *x, int size)
 {
 	for (int i = 0; i < size; i++)
 	{
 		double y = calc(a, b, x[i]);
-		char result[80];
-		sprintf_s(result, "x = %.3f y = %.3f \n", x[i], y);
-		cout << result << endl;
+		cout << x[i] << "\t" << y << endl;
 	}
 }
