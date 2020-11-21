a559b056c238d75338d2004140980567aee52f12
Creny <artem.0110@mail.ru>
2020-11-21 20:01:29 +0300

    Update Calk.h

--- CourseApp/CourseApp/Calk.h
+++ CourseApp/CourseApp/Calk.h
@@ -5,4 +5,3 @@ void Task_B(double *m, int i, double a, double b);
 void print(double a);
 double Calk(double c, double a, double b);
 void fill(int x, int y);
-void draw(int** p, int* p1, int x, int y);
