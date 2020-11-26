23fe965de2265a1fe7c00b66987c0984e656adb9
Руслан Поляков <71173176+Ruslan-Polakov@users.noreply.github.com>
2020-11-26 19:53:20 +0300

    Update CourseApp.cpp

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -2,10 +2,58 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n;
+	setlocale(LC_ALL, "Rus");
+	cout << "¬ведите длину стороны = ";
+	cin >> n;
+	int **p = new int*[n];
+	for (int i = 0; i < n; i++) {
+		p[i] = new int[n];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++) {
+			p[i][j] = 1;
+		}
+	
+	}
+	for (int i = 1; i < n-1; i++)
+	{
+		for (int j = 1; j < n-1; j++) {
+			p[i][j] = 0;
+		}
+
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+		{
+			if (i == j) {
+				p[i][j] = 1;
+			}
+
+		}
+	}
+	string s;
+	s = "*";
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (p[i][j] == 1) {
+				cout << s << ' ';
+			}
+			else {
+				cout << "  ";
+			}
+		}
+		cout << endl;
+	}
+		
+	delete[]p;
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
