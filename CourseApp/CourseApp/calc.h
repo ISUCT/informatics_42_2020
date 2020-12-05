c66f1a58fc09f79ad3dce493436f2d962fd1d09c
Mark_Rumyantsev <mrk.ru@list.ru>
2020-12-05 12:26:05 +0300

    head

--- CourseApp/CourseApp/calc.h
+++ CourseApp/CourseApp/calc.h
@@ -1,5 +1,7 @@
 #pragma once
 double calc(double a, double b, double x);
-void taskA(double xn, double xk, double dx, int* pSizeA);
+int init(double xn, double xk, double dx, int SizeA);
+void outputA(int SizeA, double* ArrayA);
+void outputB(int SizeB, double* ArrayB);
 void ScoreTaskA(double a, double b, double xn, double xk, double dx, double* ArrayA);
 void ScoreTaskB(double a, double b, double* x, int SizeB, double* ArrayB);
