c9781f9fea9fbae591dbd5e7fc449a5fc21a54b4
Mark_Rumyantsev <mrk.ru@list.ru>
2020-12-05 10:56:17 +0300

    Square 2.0

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -8,38 +8,49 @@ private:
     int offsetY = 0;
 
 public:
-    Square(int s, char n) {
+    Square(int s) {
         size = s;
         array = new char* [size];
-
+    }
+    void init(char n)
+    {
         for (int i = 0; i < size; i++) {
             array[i] = new char[size];
             for (int j = 0; j < size; j++) {
-                if (i == 0 or j == 0) {
+                if (i == 0 or j == 0)
+                {
                     array[i][j] = n;
                 }
-                else if (i == size - 1 or j == size - 1) {
+                else if (i == size - 1 || j == size - 1)
+                {
                     array[i][j] = n;
                 }
-                else {
+                else
+                {
                     array[i][j] = ' ';
                 };
             };
         };
-    }
+    };
     void setDiagonal(bool x) {
-        if (x)
+        if (x) 
+        {
             for (int i = 0; i < size; i++)
                 array[i][i] = array[0][0];
+        }
     }
 
     void print() {
         for (int i = 0; i < size + offsetY; i++) {
             for (int j = 0; j < size + offsetX; j++) {
                 if (i - offsetY < 0 or j - offsetX < 0)
+                {
                     cout << "  ";
+                }
                 else
+                {
                     cout << array[i - offsetY][j - offsetX] << ' ';
+                }
             }
             cout << '\n';
         };
@@ -53,14 +64,14 @@ public:
 
 int main()
 {
-    Square newSquare(10, '$');
+    Square newSquare(10);
 
     bool f;
     cout << "Do you want to display the diagonal? " << endl;
     cin >> f;
-
+    newSquare.init('#');
     newSquare.setDiagonal(f);
-    newSquare.setOffset(5, 4);
+    newSquare.setOffset(5, 10);
     newSquare.print();
     return 0;
 }
