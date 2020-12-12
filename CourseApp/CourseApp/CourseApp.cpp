98617c1a2df2e9534097c3d1a6637eed46c5d0e9
Mark_Rumyantsev <mrk.ru@list.ru>
2020-12-13 01:41:05 +0300

    main file

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,106 +1,11 @@
 #include <iostream>
-#include <cmath>
-#include "Calc.h"
-#include <cstdlib>
+#include "Creator.h"
 using namespace std;
-class Square {
-private:
-    int size = 0;
-    char** array;
-    int offsetX = 0;
-    int offsetY = 0;
 
-public:
-    Square(int s) {
-        size = s;
-        array = new char* [size];
-    }
-    void init(char n)
-    {
-        for (int i = 0; i < size; i++) {
-            array[i] = new char[size];
-            for (int j = 0; j < size; j++) {
-                if (i == 0 or j == 0)
-                {
-                    array[i][j] = n;
-                }
-                else if (i == size - 1 || j == size - 1)
-                {
-                    array[i][j] = n;
-                }
-                else
-                {
-                    array[i][j] = ' ';
-                };
-            };
-        };
-    };
-    void setDiagonal(bool x) {
-        if (x==1) 
-        {
-            for (int i = 0; i < size; i++)
-                array[i][i] = array[0][0];
-        }
-    }
 
-    void print() {
-        for (int i = 0; i < size + offsetY; i++) {
-            for (int j = 0; j < size + offsetX; j++) {
-                if (i - offsetY < 0 or j - offsetX < 0)
-                {
-                    cout << "  ";
-                }
-                else
-                {
-                    cout << array[i - offsetY][j - offsetX] << ' ';
-                }
-            }
-            cout << '\n';
-        };
-    };
-
-    void setOffset(int x, int y) {
-        offsetX = x;
-        offsetY = y;
-    }
-};
 
 int main()
 {
-    Square newSquare(10);
-
-    bool f;
-    cout << "Do you want to display the diagonal? " << endl;
-    cin >> f;
-    newSquare.init('#');
-    newSquare.setDiagonal(f);
-    newSquare.setOffset(5, 10);
-    newSquare.print();
-    
-
-    int SizeA = 0;
-    int SizeB = 0;
-
-    double x[] = { 1.88 ,2.26 ,3.84 ,4.55 ,-6.21 };
-
-    const double a = 0.8;
-    const double b = 0.4;
-
-    SizeA = init(1.23, 7.23, 1.2, SizeA) * 2;
-    double* ArrayA = new double[SizeA];
-
-    SizeB = ((sizeof(x) / sizeof(x[0])) * 2);
-    double* ArrayB = new double[SizeB];
-
-    ScoreTaskA(a, b, 1.23, 7.23, 1.2, ArrayA);
-    outputA(SizeA,ArrayA);
-
-    ScoreTaskB(a, b, x, SizeB, ArrayB);
-    outputB(SizeB,ArrayB);
-
-    delete[] ArrayA;
-    delete[] ArrayB;
-    return 0;
+	Mouse_Quantity();
+	return 0;
 }
-
-
