f26042427a6b60a49946f2f0af1f5c157ceb2c64
elizabetherm3@gmail.com <elizabetherm3@gmail.com>
2020-12-12 11:08:45 +0300

    Square 2

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,38 +1,44 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 class Square
 {
 private:
-	int SizeOfSquare;
+	int Compare = 0;
+	int SizeOfSquare = 0;
+	string Symbol;
 public:
 
-	Square()
+	Square(int SizeOfSquare)
 	{
-		if (SizeOfSquare > 0 && SizeOfSquare <= 50)
-		{
-			this->SizeOfSquare = SizeOfSquare;
-		}
+		this->SizeOfSquare = SizeOfSquare;
+		this->Symbol = Symbol;
 	}
-
-	void Input() // ввод размера квадрата
+	
+	void Draw(string Symbol, int compare)
 	{
-		cout << "Now you must enter the size of the square." << endl;
-		cout << "Input Size of Square:"; cin >> SizeOfSquare; cout << endl;
-	}
-
-	void Draw() // вывод квадрата на экран
-	{
-		for (int i = 1; i <= SizeOfSquare; i++)
-		{
-			for (int j = 1; j <= SizeOfSquare; j++)
+		Compare = compare;
+	
+			for (int i = 1; i <= SizeOfSquare; i++)
 			{
-				if (i == j || i == 1 || i == SizeOfSquare || j == 1 || j == SizeOfSquare)
-					cout << "*";
-				else cout << " ";
+				for (int j = 1; j <= SizeOfSquare; j++)
+				{
+					if ( i == 1 || i == SizeOfSquare || j == 1 || j == SizeOfSquare) {
+						cout << Symbol;
+					}
+					else if (Compare == 2 && i==j){
+						cout << Symbol;
+					}
+					else {
+						cout << " ";
+					}
+				}
+				cout << endl;
 			}
-			cout << endl;
-		}
+		
+
+
 	}
 
 	~Square()
@@ -42,9 +48,13 @@ public:
 
 int main()
 {
-	Square SquareOne;
-	SquareOne.Input();
-	SquareOne.Draw();
+	int compare = 1;
+	Square SquareOne(10);
+	cout << "Do you want to draw a diagonal? " << endl << "1 - No" << endl << "2 - Yes" << endl;
+	cin >> compare;
+	SquareOne.Draw("#", compare);
+
+	cout << endl;
 	return 0;
 }
 
