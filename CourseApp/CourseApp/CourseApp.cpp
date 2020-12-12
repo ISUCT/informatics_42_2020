13e54879a62d7f263e3e58dc169512843cc6585c
Varvara Troshina <troshina.vr@mail.ru>
2020-12-12 11:20:51 +0300

    taskA taskB

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,128 +1,43 @@
-//#include <iostream>
-//#include "Calc.h"
-//using namespace std;
-//
-//
-//int main(int argc, char* argv[])
-//{
-//    cout << "We have " << argc << " params" << endl;
-//    for (int i = 0; i < argc; i++)
-//    {
-//        cout << argv[i] << endl;
-//    }
-//
-//    const double a = 4.1;
-//    const double b = 2.7;
-//    double* y;
-//    int m = taskA(a, b, 1.5, 3.5, 0.4, y);
-//    for (int i = 0; i < m; i++) {
-//        cout << y[i] << endl;
-//    }
-//    delete[] y;
-//    double* y1;
-//    double x[] = { 1.9 ,2.15 ,2.34 ,2.74 ,3.16 };
-//    cout << "-----------------------" << endl;
-//    m = taskB(a, b, x, sizeof(x) / sizeof(x[0]), y1);
-//    for (int i = 0; i < m; i++) {
-//        cout << y1[i] << endl;
-//    }
-//    delete[] y1;
-//    char end;
-//    cin >> end;
-//    return 0;
-//
-//    
-//}
-
-
-//#include<iostream>
-//using namespace std;
-//int main()
-//{
-//	int n;
-//	setlocale(LC_ALL, "rus");
-//	cout << "Программа рисует квадрат звездочками со стороной \n";
-//	cout << "Введите сторону квадрата \n";
-//	cin >> n;
-//	int** p = new int* [n];
-//	for (int i = 0; i < n; i++)
-//	{
-//		p[i] = new int[n];
-//	}
-//	for (int i = 1; i <= n; i++)
-//	{
-//		for (int j = 1; j <= n; j++)
-//		{
-//			p[i][j] = n;
-//			if (i == 1 || i == n || j == 1 || j == n || i == j)
-//				cout << "**";
-//			else cout << "  ";
-//		}
-//		cout << endl;
-//	}
-//	return 0;
-//
-//}
-
 #include <iostream>
-#include <cstring>
+#include "Calc.h"
 using namespace std;
 
-class Square
+
+int main(int argc, char* argv[])
 {
-private:
-	int Compare = 0;
-	int SizeOfSquare = 0;
-	string Symbol;
-public:
+    cout << "We have " << argc << " params" << endl;
+    for (int i = 0; i < argc; i++)
+    {
+        cout << argv[i] << endl;
+    }
 
-	Square(int SizeOfSquare)
-	{
-		this->SizeOfSquare = SizeOfSquare;
-		this->Symbol = Symbol;
-	}
+    const double a = 4.1;
+    const double b = 2.7;
+    double* y;
+    int m = taskA(a, b, 1.5, 3.5, 0.4, y);
+    for (int i = 0; i < m; i++) {
+        cout << y[i] << endl;
+    }
+    delete[] y;
+    double* y1;
+    double x[] = { 1.9 ,2.15 ,2.34 ,2.74 ,3.16 };
+    cout << "-----------------------" << endl;
+    m = taskB(a, b, x, sizeof(x) / sizeof(x[0]), y1);
+    for (int i = 0; i < m; i++) {
+        cout << y1[i] << endl;
+    }
+    delete[] y1;
+    char end;
+    cin >> end;
+    return 0;
 
-	void Draw(string Symbol, int compare)
-	{
-		Compare = compare;
-
-		for (int i = 1; i <= SizeOfSquare; i++)
-		{
-			for (int j = 1; j <= SizeOfSquare; j++)
-			{
-				if (i == 1 || i == SizeOfSquare || j == 1 || j == SizeOfSquare) {
-					cout << Symbol;
-				}
-				else if (Compare == 2 && i == j) {
-					cout << Symbol;
-				}
-				else {
-					cout << " ";
-				}
-			}
-			cout << endl;
-		}
-
-
-
-	}
-
-	~Square()
-	{
-	}
-};
-
-int main()
-{
-	int compare = 1;
-	Square SquareOne(10);
-	cout << "Do you want to draw a diagonal? " << endl << "1 - No" << endl << "2 - Yes" << endl;
-	cin >> compare;
-	SquareOne.Draw("#", compare);
-
-	cout << endl;
-	return 0;
+    
 }
+
+
+
+
+
 
 
 
