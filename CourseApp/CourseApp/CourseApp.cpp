d9ad96ee31ff9c044a0da663629bba8c4d469fd6
labuser <test@test.ru>
2020-12-12 12:46:06 +0400

    e-d

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -14,11 +14,11 @@ int main()
 	double a = 2.0;
 	double b = 0.95;
 
-	//Task_A(1.25, 2.75, 0.3, a, b);
-
+	Task_A(1.25, 2.75, 0.3, a, b);
 
 	double m[5] = { 2.2, 3.78, 4.51, 6.58, 1.2 };
-	//Task_B(m, sizeof(m)/8, a, b);
+	Task_B(m, sizeof(m)/8, a, b);
+
 	for (int i = 0; i < x; i++) {
 		delete[] g[i];
 	}
