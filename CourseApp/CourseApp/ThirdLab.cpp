40be7ab0c1238467111888c57550a8db0fe762bd
Vladimir Kozhukhar <mis7akes@mail.ru>
2020-12-14 21:47:55 +0300

    Updated version of Lab3

--- CourseApp/CourseApp/ThirdLab.cpp
+++ CourseApp/CourseApp/ThirdLab.cpp
@@ -26,68 +26,67 @@ public:
             this->age = age;
         }
     }
-    string getNationality()
+    void setHealthInfo(string health)
     {
-        return nationality;
-    }
-    string getName()
-    {
-        return name;
-    }
-    string getCountryOfResidence()
-    {
-        return CountryOfResidence;
-    }
-    string getGender()
-    {
-        return gender;
-    }
-    int getAge()
-    {
-        return age;
+        this->health = health;
     }
     void DisplayInfo()
     {
         cout << "Name: " << name << "\n" << "Age: " << age << "\n" << "Gender: " << gender << endl;
-        if(nationality[0] != '\0')
+        if(nationality != "")
         {
             cout << "Nationality: " << nationality << endl;
         }
-        if(CountryOfResidence[0] != '\0')
+        if(CountryOfResidence != "")
         {
             cout << "Country: " << CountryOfResidence << endl;
+        }
+        if(health != "")
+        {
+            cout << "Your health`s condition is " << health << endl;
         }
     }
 
 private:
-    string gender = "Heavy Battle Helicopter";
+    string gender = "Heavy Battle Tank (T-92)";
     string nationality;
     string CountryOfResidence;
     string name = "NoName";
+    string health;
     int age = 0;
 };
 
 int main()
 {
-    string name,gender,nationality,CountryOfResidence;
+    string name,gender,nationality,CountryOfResidence,health;
     int age,choose;
     cout << "What is the type of the information you want to enter? (1 - Full, 0 - Main)" << endl;
     cin >> choose;
-    Person Firstbody = Person;
+    Person *person;
     if (choose == 1)
     {
-        cout << "Enter your name, age and gender" << endl;
+        cout << "Enter your name, age and gender." << endl;
         cin >> name >> age >> gender;
-        cout << "Enter your nationality and country of residence" << endl;
+        cout << "Enter your nationality and country of residence." << endl;
         cin >> nationality >> CountryOfResidence;
-        Firstbody(name,age,gender,nationality,CountryOfResidence);
+        person = new Person(name,age,gender,nationality,CountryOfResidence);
     }
     else if (choose == 0)
     {
-        cout << "Enter your name, age and gender" << endl;
+        cout << "Enter your name, age and gender." << endl;
         cin >> name >> age >> gender;
-        Firstbody(name,age,gender);
+        person = new Person(name,age,gender);
     }
+    cout << "Do you want to share information about your health?" << endl;
+    cin >> choose;
+    if(choose == 1)
+    {
+        cout << "Write your general condition." << endl;
+        cin >> health;
+        person->setHealthInfo(health);
+    }
+    person->DisplayInfo();
+    delete person;
     return 0;
 }
 
