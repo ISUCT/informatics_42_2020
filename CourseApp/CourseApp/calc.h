333d2e9165790bb99bffb9e0cbc0a232cfd4402a
Varvara Troshina <troshina.vr@mail.ru>
2020-12-19 10:55:32 +0300

    lab 3

--- CourseApp/CourseApp/calc.h
+++ CourseApp/CourseApp/calc.h
@@ -1,8 +1,8 @@
-//#pragma once
-//double calc(double a, double b, double x);
-//double calc(double x);
-//int taskA(double a, double b, double xn, double xk, double dx, double *&y);
-//int taskB(double a, double b, double* x, int size, double *&y1);
+#pragma once
+double calc(double a, double b, double x);
+double calc(double x);
+int taskA(double a, double b, double xn, double xk, double dx, double *&y);
+int taskB(double a, double b, double* x, int size, double *&y1);
 
 
 
