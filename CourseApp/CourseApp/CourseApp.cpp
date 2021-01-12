5622736225cdae7b712b5bfe78f25660bcfecb4f
Varvara Troshina <troshina.vr@mail.ru>
2021-01-12 11:10:07 +0300

    5 var

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,160 +1,69 @@
-#include <iostream>
-#include <math.h>
-#include "Calc.h"
-using namespace std;
-
-int main(int argc, char* argv[])
-{
-    cout << "We have " << argc << " params" << endl;
-    for (int i = 0; i < argc; i++)
-    {
-        cout << argv[i] << endl;
-    }
-
-    const double a = 4.1;
-    const double b = 2.7;
-    double* y;
-    int m = taskA(a, b, 1.5, 3.5, 0.4, y);
-    for (int i = 0; i < m; i++) {
-        cout << y[i] << endl;
-    }
-    delete[] y;
-    double* y1;
-    double x[] = { 1.9 ,2.15 ,2.34 ,2.74 ,3.16 };
-    cout << "-----------------------" << endl;
-    m = taskB(a, b, x, sizeof(x) / sizeof(x[0]), y1);
-    for (int i = 0; i < m; i++) {
-        cout << y1[i] << endl;
-    }
-    delete[] y1;
-    char end;
-    cin >> end;
-    return 0;
-
-    
-}
-
-
 
 #include <iostream>
-#include <cstring>
 using namespace std;
 
-class Square
+class massive
 {
 private:
-	int Compare = 0;
-	int SizeOfSquare = 0;
-	string Symbol;
+	int size;
+
 public:
-
-	Square(int SizeOfSquare)
+	massive(int s)
 	{
-		this->SizeOfSquare = SizeOfSquare;
-		this->Symbol = Symbol;
-	}
+		size = s;
 
-	void Draw(string Symbol, int compare)
-	{
-		Compare = compare;
+		int* l = new int[size];
 
-		for (int i = 1; i <= SizeOfSquare; i++)
-		{
-			for (int j = 1; j <= SizeOfSquare; j++)
-			{
-				if (i == 1 || i == SizeOfSquare || j == 1 || j == SizeOfSquare) {
-					cout << Symbol;
-				}
-				else if (Compare == 2 && i == j) {
-					cout << Symbol;
-				}
-				else {
-					cout << " ";
-				}
-			}
-			cout << endl;
+		cout << endl << "enter massive:" << endl;
+		for (int i = 0; i < size; ++i) {
+			cout << "a[" << i << "]=";
+			cin >> l[i];
 		}
 
+		quick_sort(l, 0, size - 1);
 
+		int k = 1;
+
+		for (int i = size - 1; i > size - 3; --i) {
+			k = k * l[i];
+			cout << endl << "a[" << i << "]=" << l[i] << " —-max" << endl;
+		}
+		cout << endl << "Proizvedenie max =" << k << endl;
+	}
+	void quick_sort(int* a, int min, int max) {
+		int i = min;
+		int j = max;
+		int x = a[(min + max) / 2];
+		int t;
+		do {
+			while (a[i] < x) {
+				++i;
+			}
+			while (a[j] > x) {
+				--j;
+			}
+			if (i <= j) {
+				t = a[i];
+				a[i] = a[j];
+				a[j] = t;
+				i++; j--;
+			}
+		} while (i <= j);
+
+		if (min < j) {
+			quick_sort(a, min, j);
+		};
+		if (i < max) {
+			quick_sort(a, i, max);
+		}
 
 	}
-
-	~Square()
-	{
-	}
-};
-
-int main()
-{
-	int compare = 1;
-	Square SquareOne(10);
-	cout << "Do you want to draw a diagonal? " << endl << "1 - No" << endl << "2 - Yes" << endl;
-	cin >> compare;
-	SquareOne.Draw("#", compare);
-
-	cout << endl;
-	return 0;
-}
-
-		
-
-
-
-	
-
-
-
-
-
-
-
-#include <iostream>
-#include <string>
-using namespace std;
-class EMPLOYEE
-{
-public:
-	string name;
-	string surname;
-	string patronymic;
-	int ident;
-	string data;
-	EMPLOYEE(string s, string n, string p, int a, string d)
-	{
-		surname = s; name = n; ident = a; data = d;   patronymic = p;
-	}
-	void display()
-	{
-		cout << "_____________________________" << endl;
-		cout << "FULL NAME:  " << surname << " ";
-		cout << name << " ";
-		cout << patronymic << endl;
-		cout << "IDENT " << ident << endl;
-		cout << "DATE: " << data << endl;
-		cout << "_____________________________" << endl;
-		cout << endl;
-	}
-	~EMPLOYEE() {};
 };
 int main()
 {
-	EMPLOYEE ONE = EMPLOYEE("LOPPY", "D.", "L.", 22, "08/09/15");
-	EMPLOYEE TWO = EMPLOYEE("HERRANY", "A.", "R.", 21, "18/10/14");
-	EMPLOYEE THREE = EMPLOYEE("MILAN", "S.", "J.", 19, "06/07/19");
-	EMPLOYEE FOUR = EMPLOYEE("POOL", "P.", "Q.", 41, "01/09/04");
-	EMPLOYEE FIVE = EMPLOYEE("POTO", "E.", "O.", 28, "03/11/14");
-	EMPLOYEE SIX = EMPLOYEE("OSSO", "O.", "L.", 32, "28/10/12");
-	EMPLOYEE SEVEN = EMPLOYEE("ALEXEEV", "I.", "A.", 23, "21/03/10");
-	EMPLOYEE EIGHT = EMPLOYEE("ROND", "T.", "E.", 24, "26/01/14");
-	EMPLOYEE NINE = EMPLOYEE("LU", "P.", "K.", 36, "01/10/10");
-	ONE.display();
-	TWO.display();
-	THREE.display();
-	FOUR.display();
-	FIVE.display();
-	SIX.display();
-	SEVEN.display();
-	EIGHT.display();
-	NINE.display();
-	return 0;
+	int size;
+	cout << "enter size:";
+	cin >> size;
+	massive massive(size);
+
 }
