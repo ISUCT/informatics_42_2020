c912dbf5bc3edd91f5b756a50b100f04d701c7c6
Alexander Seleznev <fizika10a2@yandex.ru>
2021-01-12 10:39:38 +0300

    Added class

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -2,6 +2,55 @@
 #include <string>
 using namespace std;
 
+class Pistol {
+private:
+	string model;
+	int caliber;
+	int year_of_release;
+	int clip_capacity;
+public:
+	int number_of_bullets;
+	int shoot = 0;
+
+
+	void Input() {
+
+			cout << "Модель: "; cin >> model; cout << endl;
+			cout << "Калибр: "; cin >> caliber; cout << endl;
+			cout << "Год выпуска: "; cin >> year_of_release; cout << endl;
+			cout << "Вместимость обоймы: "; cin >> clip_capacity; cout << endl;
+	}
+
+	void Reload() {
+
+		number_of_bullets = clip_capacity;
+		cout << "Пистолет перезаряжен" << endl;
+	}
+
+
+
+	void Shoot() {
+		cout << "Сколько патронов в пистолете?:"; cin >> number_of_bullets;
+		while (shoot < 1) {
+			if (number_of_bullets > clip_capacity) {
+				cout << "Невозможно, введите другое число"; cin >> number_of_bullets;
+
+			}
+			if (number_of_bullets < 1) {
+				cout << "Нет патронов, перезарядите пистолет" << endl;
+				Reload();
+			}
+			else {
+				cout << "Выстрел произведён" << endl;
+				shoot++;
+			}
+
+		}
+	}
+
+
+};
+
 int main() {
 
 	setlocale(LC_ALL, "Rus");
@@ -73,6 +122,16 @@ int main() {
 		}
 
 		delete[]p;
-		return 0;
 
+		Pistol Glock_17;
+		Pistol PM;
+
+		Glock_17.Input();
+		Glock_17.Shoot();
+		cout << endl;
+
+		PM.Input();
+		PM.Shoot();
+
+		return 0;
 }
